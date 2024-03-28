e3df21ba8156570e30e60305b6dedfab4e683f1d
Besttttt11 <150612869+Besttttt11@users.noreply.github.com>
2024-03-28 11:35:54 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Website Developing using Python")
 st.header("🐡 Website Developing using Python 🐡")
 st.subheader("🐡 Teerapat Buapradit 🐡")
 st.image('cat.jpg')
+
+dt=pd.read_csv('./data/iris.csv')
+st.write(dt.head(10))
