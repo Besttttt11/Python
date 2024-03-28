6695a7a81e3317edf0516230cb90ce59e626c37d
Besttttt11 <150612869+Besttttt11@users.noreply.github.com>
2024-03-28 11:31:17 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🐡🐡 Website Developing using Python 🐡🐡")
+st.title("🐡 Website Developing using Python 🐡")
 st.header("Website Developing using Python")
 st.subheader("Teerapat Buapradit")
 st.image('cat.jpg')
