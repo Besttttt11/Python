e092856090e1deb8dd4263e2869d247233a9b954
Besttttt11 <150612869+Besttttt11@users.noreply.github.com>
2024-03-28 11:20:58 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -3,4 +3,4 @@ import streamlist as st
 st.title("Website Developing using Python")
 st.header("Website Developing using Python")
 st.subheader("Teerapat Buapradit")
-st.amg('cat.jpg')
+st.image('cat.jpg')
