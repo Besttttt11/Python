53599af97758917034c3da3fff36f270ede7747c
Besttttt11 <150612869+Besttttt11@users.noreply.github.com>
2024-03-28 11:34:03 +0700

    import data

--- MyApp1.py
+++ MyApp1.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Website Developing using Python")
 st.header("🐡 Website Developing using Python 🐡")
-st.subheader("Teerapat Buapradit")
+st.subheader("🐡 Teerapat Buapradit 🐡")
 st.image('cat.jpg')
